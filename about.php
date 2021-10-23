62c893235352700280256de6fd3980697f5592d5
vaishakmk <37268852+vaishakmk@users.noreply.github.com>
2021-10-22 23:56:49 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>About Us</title>
+    <link rel="icon" href="images/imglogo.jpeg">
+    <link rel="stylesheet" href = "css/style.css">
+
+    <style>
+
+        .about-main{
+                width: 100%;
+                height: 2200px;
+                position: relative;
+                background: linear-gradient(rgba(0,0,50,0.5),rgba(0,0,50,0.5)),url(images/background2.jpg);
+                background-attachment: fixed;
+                background-size: cover;
+                background-position: center;        
+        }
+        .about-menu a{
+               font-weight:900px;
+                color:bisque;
+        }
+      
+        .navbar-brand{
+                color:bisque;
+        }
+
+
+
+        .floating-btn{
+      width : 80px;
+      height : 80px;
+      background: bisque;
+      display: flex;
+      align-items: center;
+      justify-content : center;
+      text-decoration: none;
+      border-radius : 50%;
+      color: black;
+      font-size : 20px; 
+      box-shadow: 2px 2px 5px rgba(0,0,0,0.25);
+      position:fixed;
+      right : 20px;
+      bottom:40%;
+      transition : background 0.25s;
+    }
+
+    .floating-btn:hover{
+      color: black;
+    }
+
+
+    .floating-btn:active{
+      background: BROWN ;
+  
+    }
+
+
+.container {
+  position: relative;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.container img {vertical-align: middle;margin: 50px;}
+
+.container .content {
+  position: absolute;
+  bottom: 50px;
+  background: rgb(0, 0, 0); /* Fallback color */
+  background: rgba(0, 0, 0, 0.5); /* Black background with 0.5 opacity */
+  color: #f1f1f1;
+  width: 97%;
+  left: 60px;
+  padding: 20px;
+}
+</style>
+
+
+
+
+  </head>
+  <body>
+
+    <section class = "about-main">
+      <nav>
+        <a class="navbar-brand" href="#">
+          <img src = "images/imglogo.jpeg" width = "90" height = "90" /> B&M
+        </a>
+
+        <ul class= "about-menu">
+          <li><a href="index.php"  >Home</a><li>
+          <li><a href="about.php" class = "active">About</a><li>
+          <li><a href="services.php">Services</a><li>
+          <li><a href="news.php">News</a><li>
+          <li><a href="contacts.php">Contacts</a><li>
+          <li><a href="user.php">Users</a><li>
+        </ul>
+      </nav>
+
+      <a href="modal.php" class="material-icons floating-btn">LOGIN</a>
+    
+<h2 style='color:bisque'> Enjoy Award-Winning Locally Roasted Coffee in the Company of our Furry Friends! </h2>
+
+
+<div class="container">
+  <img src="images/img1.jpeg" alt="Notebook" style="width:100%;">
+  <div class="content">
+    
+    <p> If you are not sure that you want a pet or not, we can help you decide.
+    </p>
+  </div>
+</div>
+
+<div class="container">
+  <img src="images/child&dog.jpeg" alt="Notebook" style="width:100%;">
+  <div class="content">
+    
+    <p>All our pets here are well-trained and totally friendly to anyone including children and other pets too.</p>
+  </div>
+</div>
+
+<div class="container">
+  <img src="images/img13.jpeg" alt="Notebook" style="width:100%;">
+  <div class="content">
+    
+    <p> Pets, especially dogs and cats, can reduce stress, anxiety, and depression, ease loneliness, encourage exercise and playfulness, 
+      and even improve your cardiovascular health. Caring for an animal can help children grow up more secure and active.</p>
+  </div>
+</div>
+</section>
+    </body>
+    </html>
---
+++ contacts.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Contacts</title>
+    <link rel="icon" href="images/imglogo.jpeg">
+    <link rel="stylesheet" href = "css/style.css">
+
+    <style>
+        .contact-container{
+            margin-left: 90px;
+            color:bisque;
+        }
+        
+        .contacts-main h1{
+            margin-top:50px;
+            margin-left:50px;
+            font-weight: 900px;
+            color:bisque;
+            text-decoration:underline;
+        }
+
+        .contacts-main{
+                width: 100%;
+                height: 700px;
+                position: relative;
+                background: linear-gradient(rgba(0,0,50,0.5),rgba(0,0,50,0.5)),url(images/img2.jpeg);
+                background-attachment: fixed;
+                background-size: cover;
+                background-position: center;
+        }
+
+        .contacts-menu a{
+               font-weight:900px;
+                color:bisque;
+        }
+      
+        .navbar-brand{
+                color:bisque;
+        }
+
+        nav ul li a:hover{
+    background-color: rgb(178, 34, 34, 0.793);
+    color: bisque;
+    box-shadow: 5px 10px 15px rgba(134, 49, 0, 0.398);
+        }
+.active{
+  background-color: rgb(178, 34, 34, 0.616);
+    color: bisque;
+    box-shadow: 5px 10px 15px rgba(134, 49, 0, 0.398);
+}
+
+
+.floating-btn{
+      width : 80px;
+      height : 80px;
+      background: rgb(178, 34, 34);
+      display: flex;
+      align-items: center;
+      justify-content : center;
+      text-decoration: none;
+      border-radius : 50%;
+      color: bisque;
+      font-size : 20px; 
+      box-shadow: 2px 2px 5px rgba(0,0,0,0.25);
+      position:fixed;
+      right : 20px;
+      bottom:40%;
+      transition : background 0.25s;
+    }
+
+    .floating-btn:hover{
+      color: bisque;
+    }
+
+
+    .floating-btn:active{
+      background: BROWN ;
+      text-color: bisque;
+    }
+
+
+
+        </style>
+
+  </head>
+  <body>
+    <section class = "contacts-main">
+      <nav>
+        <a class="navbar-brand" href="#">
+          <img src = "images/imglogo.jpeg" width = "90" height = "90" /> B&M
+        </a>
+
+        <ul class= "contacts-menu">
+          <li><a href="index.php" >Home</a><li>
+          <li><a href="about.php" >About</a><li>
+          <li><a href="services.php">Services</a><li>
+          <li><a href="news.php">News</a><li>
+          <li><a href="contacts.php" class = "active">Contacts</a><li>
+          <li><a href="user.php">Users</a><li>
+        </ul>
+      </nav>
+
+
+      <a href="modal.php" class="material-icons floating-btn">LOGIN</a>
+    <h1> CONTACT US </h1>
+<div class="contact-container">
+    <?php
+
+        function phpReadFile($filename){
+            $myfile = fopen($filename,"r") or die("Unable to open file !");
+            $filecontent = fread($myfile,filesize($filename));
+            fclose($myfile);
+            return $filecontent;
+        }
+
+        echo nl2br(phpReadFile("contacts.txt"));
+
+    ?>
+
+</div>
+
+
+
+      </section>
+    </body>
+</html>
